110a97e70913fb67609ad99651d9ad2ffaaabc96
CesarNS19 <cesarneri803@gmail.com>
2025-01-28 16:02:58 -0600

    cambios

---
+++ mysql/connection.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost"; 
+$username = "root";        
+$password = "";            
+$database = "muebleria";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+}
+$conn->set_charset("utf8");
+
+?>
