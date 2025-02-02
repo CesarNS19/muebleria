eaaafd56698c54716538be85088f453506fca867
CesarNS19 <cesarneri803@gmail.com>
2025-02-02 16:20:38 -0600

    cambios

--- src/admin/products.php
+++ src/admin/products.php
@@ -19,3 +19,121 @@ $title = "Muebleria ┃ Admin Products";
             Agregar Producto
         </button><br/>
     </section><br/>
+
+
+<!-- Tabla de Productos -->
+<section class="products-table container my-4">
+    <h2 class="text-center mb-4">Administrar Productos</h2>
+    <div class="table-responsive">
+        <table class="table table-bordered table-hover text-center">
+            <thead class="thead-dark">
+                <tr>
+                    <th>Producto</th>
+                    <th>Descripción</th>
+                    <th>Precio</th>
+                    <th>Stock</th>
+                    <th>Color</th>
+                    <th>Tamaño</th>
+                    <th>Capacidad</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $sql = "SELECT * FROM productos";
+                    $result = $conn->query($sql);
+                    
+                    if ($result->num_rows > 0) {
+                        while ($row = $result->fetch_assoc()) {
+                            echo "<tr>";
+                            echo "<td>" . htmlspecialchars($row['nombre']) . "</td>";
+                            echo "<td>" . htmlspecialchars($row['descripcion']) . "</td>";
+                            echo "<td>". htmlspecialchars($row['precio']). "</td>";
+                            echo "<td>". htmlspecialchars($row['stock']). "</td>";
+                            echo "<td>". htmlspecialchars($row['color']). "</td>";
+                            echo "<td>". htmlspecialchars($row['tamaño']). "</td>";
+                            echo "<td>". htmlspecialchars($row['capacidad']). "</td>";
+                            echo "<td>";
+                            echo "<button class='btn btn-info btn-sm me-1' onclick='openEditModal(" . json_encode($row) . ")' title='Editar Producto'>
+                                    <i class='fas fa-edit'></i>
+                                </button>
+                                <a href='products/delete_products.php?id=" . $row['id_producto'] . "' class='btn btn-danger btn-sm' onclick='return confirm(\"¿Estás seguro de que deseas eliminar este producto?\")' title='Eliminar Producto'>
+                                    <i class='fas fa-trash'></i>
+                                </a>";
+                            echo "</td>";
+                            echo "</tr>";
+                        }
+                    } else {
+                        echo "<tr><td colspan='6'>No se encontrarón productos</td></tr>";
+                    }
+                ?>
+            </tbody>
+        </table>
+    </div>
+</section>
+
+<script>
+
+    function openEditModal(categoriesData) {
+        $('#edit_id_marca').val(categoriesData.id_marca);
+        $('#edit_nombre').val(categoriesData.nombre);
+        $('#edit_descripcion').val(categoriesData.descripcion);    
+        $('#editBrandsModal').modal('show');
+    }
+
+    function mostrarToast(titulo, mensaje, tipo) {
+            let icon = '';
+            let alertClass = '';
+
+            switch (tipo) {
+                case 'success':
+                    icon = '<span class="fas fa-check-circle text-white fs-6"></span>';
+                    alertClass = 'alert-success';
+                    break;
+                case 'error':
+                    icon = '<span class="fas fa-times-circle text-white fs-6"></span>';
+                    alertClass = 'alert-danger';
+                    break;
+                case 'warning':
+                    icon = '<span class="fas fa-exclamation-circle text-white fs-6"></span>';
+                    alertClass = 'alert-warning';
+                    break;
+                case 'info':
+                    icon = '<span class="fas fa-info-circle text-white fs-6"></span>';
+                    alertClass = 'alert-info';
+                    break;
+                default:
+                    icon = '<span class="fas fa-info-circle text-white fs-6"></span>';
+                    alertClass = 'alert-info';
+                    break;
+            }
+
+            const alert = `
+            <div class="alert ${alertClass} d-flex align-items-center alert-dismissible fade show" role="alert">
+                <div class="me-2">${icon}</div>
+                <div>${titulo}: ${mensaje}</div>
+                <button type="button" class="btn-close ms-auto" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>`;
+
+            $("#Alert").html(alert);
+
+            setTimeout(() => {
+                $(".alert").alert('close');
+            }, 4000);
+        }
+
+        document.addEventListener('DOMContentLoaded', function() {
+            <?php if (isset($_SESSION['status_message']) && isset($_SESSION['status_type'])): ?>
+                <?php if ($_SESSION["status_type"] === "warning"): ?>
+                    mostrarToast("Advertencia", '<?= $_SESSION["status_message"] ?>', '<?= $_SESSION["status_type"] ?>');
+                <?php elseif ($_SESSION["status_type"] === "error"): ?>
+                    mostrarToast("Error", '<?= $_SESSION["status_message"] ?>', '<?= $_SESSION["status_type"] ?>');
+                <?php elseif ($_SESSION["status_type"] === "info"): ?>
+                    mostrarToast("Info", '<?= $_SESSION["status_message"] ?>', '<?= $_SESSION["status_type"] ?>');
+                <?php else: ?>
+                    mostrarToast("Éxito", '<?= $_SESSION["status_message"] ?>', '<?= $_SESSION["status_type"] ?>');
+                <?php endif; ?>
+                <?php unset($_SESSION['status_message'], $_SESSION['status_type']); ?>
+            <?php endif; ?>
+        });
+</script>
