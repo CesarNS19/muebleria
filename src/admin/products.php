c5162f3fa59d90bbc39f07195cf14bd72a24d51b
CesarNS19 <cesarneri803@gmail.com>
2025-02-03 10:16:03 -0600

    cambios

--- src/admin/products.php
+++ src/admin/products.php
@@ -15,11 +15,184 @@ $title = "Muebleria ┃ Admin Products";
 <div id="Alert"></div>
 
 <section class="company-header">
-        <button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#addProductModal" style="float: right; margin: 10px;">
+        <button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#addProductsModal" style="float: right; margin: 10px;">
             Agregar Producto
         </button><br/>
     </section><br/>
 
+<!-- Modal para añadir productos -->
+<div class="modal fade" id="addProductsModal" tabindex="-1" role="dialog" aria-labelledby="addProductsModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="addProductsModalLabel">Agregar Nuevo Producto</h5>
+            </div>
+            <form action="products/add_product.php" method="POST" enctype="multipart/form-data">
+                <div class="modal-body">
+                <div class="form-group mb-3">
+                    <label>Categoría</label>
+                    <select name="id_categoria" class="form-control" required>
+                        <option value="">Seleccione una categoría</option>
+                        <?php
+                        $categoria_sql = "SELECT * FROM categorias";
+                        $categoria_result = $conn->query($categoria_sql);
+
+                        while ($categoria = $categoria_result->fetch_assoc()) {
+                            echo "<option value='" . $categoria['id_categoria'] . "'>" . htmlspecialchars($categoria['nombre']) . "</option>";
+                        }
+                        ?>
+                    </select>
+                </div>
+                <div class="form-group mb-3">
+                    <label>Marca</label>
+                    <select name="id_marca" class="form-control" required>
+                        <option value="">Seleccione una marca</option>
+                        <?php
+                        $marca_sql = "SELECT * FROM marcas";
+                        $marca_result = $conn->query($marca_sql);
+
+                        while ($marca = $marca_result->fetch_assoc()) {
+                            echo "<option value='" . $marca['id_marca'] . "'>" . htmlspecialchars($marca['nombre']) . "</option>";
+                        }
+                        ?>
+                    </select>
+                </div>
+                    <div class="form-group mb-3">
+                        <label for="">Nombre del producto</label>
+                        <input type="text" name="nombre" class="form-control" required>
+                    </div>
+                    <div class="form-group mb-3">
+                        <label for="">Descripción</label>
+                        <textarea name="descripcion" class="form-control" required></textarea>
+                    </div>
+                    <div class="form-group mb-3">
+                        <label for="">Precio</label>
+                        <input type="number" name="precio" class="form-control" required>
+                    </div>
+                    <div class="form-group mb-3">
+                        <label for="">Stock</label>
+                        <input type="number" name="stock" class="form-control" required>
+                    </div>
+                    <div class="form-group mb-3">
+                        <label for="">Color</label>
+                        <input type="text" name="color" class="form-control" required>
+                    </div>
+                    <div class="form-group mb-3">
+                        <label for="">Tamaño</label>
+                        <input type="text" name="tamaño" class="form-control" required>
+                    </div>
+                    <div class="form-group mb-3">
+                        <label for="">Capacidad</label>
+                        <input type="text" name="capacidad" class="form-control" required>
+                    </div>
+                    <div class="form-group mb-3">
+                        <input type="file" name="imagen_producto" class="form-control" placeholder="Imágen del producto">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                    <button type="submit" class="btn btn-primary">Agregar Producto</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<!-- Modal para editar productos -->
+<div class="modal fade" id="editProductsModal" tabindex="-1" role="dialog" aria-labelledby="editProductsLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="editProductsLabel">Editar Producto</h5>
+            </div>
+            <form action="products/edit_product.php" method="POST" enctype="multipart/form-data">
+                <div class="modal-body">
+                    <input type="hidden" name="id_producto" id="edit_id_producto">
+                    
+                    <div class="form-group mb-3">
+                        <label for="edit_categoria">Categoría</label>
+                        <select name="id_categoria" id="edit_categoria" class="form-control" required>
+                            <?php
+                            $categoria_sql = "SELECT * FROM categorias";
+                            $categoria_result = $conn->query($categoria_sql);
+
+                            while ($categoria = $categoria_result->fetch_assoc()) {
+                                echo "<option value='" . $categoria['id_categoria'] . "'>" . htmlspecialchars($categoria['nombre']) . "</option>";
+                            }
+                            ?>
+                        </select>
+                    </div>
+
+                    <div class="form-group mb-3">
+                        <label for="edit_marca">Categoría</label>
+                        <select name="id_marca" id="edit_marca" class="form-control" required>
+                            <?php
+                            $categoria_sql = "SELECT * FROM marcas";
+                            $categoria_result = $conn->query($categoria_sql);
+
+                            while ($categoria = $categoria_result->fetch_assoc()) {
+                                echo "<option value='" . $categoria['id_marca'] . "'>" . htmlspecialchars($categoria['nombre']) . "</option>";
+                            }
+                            ?>
+                        </select>
+                    </div>
+
+                    <div class="form-group mb-3">
+                        <label for="edit_nombre">Nombre del producto</label>
+                        <input type="text" name="nombre" id="edit_nombre" class="form-control" required>
+                    </div>
+
+                    <div class="form-group mb-3">
+                        <label for="edit_descripcion">Descripción del producto</label>
+                        <textarea name="descripcion" class="form-control" id="edit_descripcion" required></textarea>
+                    </div>
+
+                    <div class="form-group mb-3">
+                        <label for="edit_precio">Precio del Producto</label>
+                        <input type="number" name="precio" id="edit_precio" class="form-control" required>
+                    </div>
+
+                    <div class="form-group mb-3">
+                        <label for="edit_stock">Stock</label>
+                        <input type="number" name="stock" id="edit_stock" class="form-control" required>
+                    </div>
+
+                    <div class="form-group mb-3">
+                        <label for="edit_color">Color</label>
+                        <input type="text" name="color" id="edit_color" class="form-control" required>
+                    </div>
+
+                    <div class="form-group mb-3">
+                        <label for="edit_tamaño">Tamaño</label>
+                        <input type="text" name="tamaño" id="edit_tamaño" class="form-control" required>
+                    </div>
+
+                    <div class="form-group mb-3">
+                        <label for="edit_capacidad">Capacidad</label>
+                        <input type="text" name="capacidad" id="edit_capacidad" class="form-control" required>
+                    </div>
+
+                    <div class="form-group mb-3">
+                        <label>Imágen Actual</label>
+                        <div>
+                            <img id="current_image" src="" width="100" alt="Imágen del producto">
+                        </div>
+                    </div>
+
+                    <div class="form-group mb-3">
+                        <label for="edit_imagen">Actualizar Imágen del Producto</label>
+                        <input type="file" name="imagen_producto" id="edit_imagen" class="form-control">
+                    </div>
+                </div>
+
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                    <button type="submit" class="btn btn-primary">Guardar Cambios</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
 
 <!-- Tabla de Productos -->
 <section class="products-table container my-4">
@@ -35,6 +208,7 @@ $title = "Muebleria ┃ Admin Products";
                     <th>Color</th>
                     <th>Tamaño</th>
                     <th>Capacidad</th>
+                    <th>Imágen</th>
                     <th>Acciones</th>
                 </tr>
             </thead>
@@ -53,11 +227,12 @@ $title = "Muebleria ┃ Admin Products";
                             echo "<td>". htmlspecialchars($row['color']). "</td>";
                             echo "<td>". htmlspecialchars($row['tamaño']). "</td>";
                             echo "<td>". htmlspecialchars($row['capacidad']). "</td>";
+                            echo "<td><img src='img/" . htmlspecialchars($row['imagen']) . "' width='100px' height='60px' alt='Imágen Producto'></td>";
                             echo "<td>";
                             echo "<button class='btn btn-info btn-sm me-1' onclick='openEditModal(" . json_encode($row) . ")' title='Editar Producto'>
                                     <i class='fas fa-edit'></i>
                                 </button>
-                                <a href='products/delete_products.php?id=" . $row['id_producto'] . "' class='btn btn-danger btn-sm' onclick='return confirm(\"¿Estás seguro de que deseas eliminar este producto?\")' title='Eliminar Producto'>
+                                <a href='products/delete_product.php?id=" . $row['id_producto'] . "' class='btn btn-danger btn-sm' onclick='return confirm(\"¿Estás seguro de que deseas eliminar este producto?\")' title='Eliminar Producto'>
                                     <i class='fas fa-trash'></i>
                                 </a>";
                             echo "</td>";
@@ -74,11 +249,19 @@ $title = "Muebleria ┃ Admin Products";
 
 <script>
 
-    function openEditModal(categoriesData) {
-        $('#edit_id_marca').val(categoriesData.id_marca);
-        $('#edit_nombre').val(categoriesData.nombre);
-        $('#edit_descripcion').val(categoriesData.descripcion);    
-        $('#editBrandsModal').modal('show');
+    function openEditModal(productsData) {
+        $('#edit_id_producto').val(productsData.id_producto);
+        $('#edit_categoria').val(productsData.id_categoria);
+        $('#edit_id_marca').val(productsData.id_marca);
+        $('#edit_nombre').val(productsData.nombre);
+        $('#edit_descripcion').val(productsData.descripcion);  
+        $('#edit_precio').val(productsData.precio);
+        $('#edit_stock').val(productsData.stock);
+        $('#edit_color').val(productsData.color);
+        $('#edit_tamaño').val(productsData.tamaño);
+        $('#edit_capacidad').val(productsData.capacidad);  
+        $('#current_image').attr('src', 'img/' + productsData.imagen);
+        $('#editProductsModal').modal('show');
     }
 
     function mostrarToast(titulo, mensaje, tipo) {
