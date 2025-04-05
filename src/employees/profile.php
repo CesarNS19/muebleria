bee71c73afc4aa5f9ce15537215ffbaa870fdf3e
CesarNS19 <cesarneri803@gmail.com>
2025-04-04 20:36:59 -0600

    cambios

--- src/employees/profile.php
+++ src/employees/profile.php
@@ -14,17 +14,9 @@ $result = $stmt->get_result();
 $userData = $result->fetch_assoc();
 ?>
 
-<style>
-    .container{
-        background-color: white;
-    }
-</style>
-
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 <title><?php echo $title; ?></title>
 
 <!-- Card para mostrar información del empleado-->
